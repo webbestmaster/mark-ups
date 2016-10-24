1d1bdffe03c079441ccb2ff585e2becc6d849c47
dim <web.best.master@gmail.com>
2016-10-24 23:22:05 +0300

    all-deals - in progress...

--- handover_01/www/js/main.js
+++ handover_01/www/js/main.js
@@ -1,6 +1,34 @@
+(function (win, doc) {
 
-(function () {
+    'use strict';
 
-    console.log('I am loaded!!!');
+    function bindScrollToNode() {
 
-}());
+        function onClickScrollToNode(e) {
+
+            e.preventDefault();
+
+            var node = e.currentTarget,
+                selector = '.' + node.getAttribute('data-js-scroll-to'),
+                targetNode = doc.querySelector(selector);
+
+            targetNode.scrollIntoView();
+
+        }
+
+        var selector = '.js-scroll-to-node',
+            nodes = doc.querySelectorAll(selector),
+            node,
+            i, len;
+
+        for (i = 0, len = nodes.length; i < len; i += 1) {
+            nodes[i].addEventListener('click', onClickScrollToNode, false);
+        }
+
+    }
+
+    win.addEventListener('load', function () {
+        bindScrollToNode();
+    }, false);
+
+}(window, window.document));

7ff32405d2db17e6ff80f8c1170028c5403be025
dim <web.best.master@gmail.com>
2016-10-29 22:17:07 +0300

    BFC - mobile

--- handover_01/www/js/slider.js
+++ handover_01/www/js/slider.js
@@ -22,21 +22,35 @@
             spaceBetween: 0,
             grabCursor: false,
             preventClicks: false,
-            centeredSlides: true // diff of normal
+            // diff of normal
+            centeredSlides: true
+        },
+        double: {
+            scrollbar: '.swiper-scrollbar',
+            scrollbarHide: true,
+            slidesPerView: 'auto',
+            spaceBetween: 0,
+            grabCursor: false,
+            preventClicks: false,
+            // diff of normal
+            nextButton: '.horizontal-slider__right-arrow',
+            prevButton: '.horizontal-slider__left-arrow'
         }
     };
 
     function init() {
 
-        var curState = doc.documentElement.clientWidth >= desctopWidth ? 'desctop' : 'mobile';
+        var curState = doc.documentElement.clientWidth >= desctopWidth ? 'desktop' : 'mobile';
 
         if (prevState === curState) { // do not anything if state is equal
             return;
         }
 
+        initDoubleSwiper();
+
         prevState = curState;
 
-        if (curState === 'desctop') {
+        if (curState === 'desktop') {
             destroy();
             return;
         }
@@ -63,6 +77,53 @@
         swipers.forEach(function (swiper) {
             swiper.destroy(false, true);
         });
+    }
+
+    var doubleSwipers = [];
+
+    function initDoubleSwiper() {
+
+        doubleSwipers.forEach(function (swiper) {
+            swiper.destroy(false, true);
+        });
+
+        var platform = doc.documentElement.clientWidth >= desctopWidth ? 'desktop' : 'mobile',
+            nodes = $('.js-double-horizontal-scroll--centered-slides');
+
+
+        if (platform === 'mobile') {
+            doubleSwipers = Array.prototype.map.call(nodes, function (node) {
+                var $node = $(node);
+                return new Swiper($node, cfg.centered);
+            });
+        }
+
+        if (platform === 'desktop') {
+            doubleSwipers = Array.prototype.map.call(nodes, function (node) {
+                var $node = $(node);
+                return new Swiper($node, cfg.double);
+            });
+        }
+
+/*
+        switch (platform) {
+
+            case 'desktop':
+
+                break;
+
+            case 'mobile':
+
+                break;
+
+            default:
+                console.log('WAT?!');
+
+        }
+*/
+
+
+
     }
 
     $(window).on('load resize', init);

ed6217d6d0f34d6de8a59d838a0020a040c2c9ee
dim <web.best.master@gmail.com>
2016-10-31 22:47:51 +0300

    Single dial - add "open/close" sections - fix

---
+++ handover_01/www/js/show-hide-blocks.js
@@ -0,0 +1,13 @@
+(function () {
+
+    function init() {
+
+        $('.js-show-hide-info-block').on('click', function () {
+            $(this).parent().toggleClass('deal-data-card--open-card');
+        });
+
+    }
+
+    $(window).on('load', init);
+
+}());

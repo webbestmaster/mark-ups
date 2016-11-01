11c566299f8c54dd0bdee7d9a405e36158388064
dim <web.best.master@gmail.com>
2016-11-01 20:33:32 +0300

    Single deal page. Proposals. - add missed "show\hide" buttons for mobile

---
+++ handover_01/www/js/show-hide-details.js
@@ -0,0 +1,17 @@
+(function () {
+
+    function init() {
+
+        $('.js-show-hide-details--show').on('click', function () {
+            $(this).closest('.js-show-hide-details__parent').addClass('deal-option-card__details--show-interactive-block');
+        });
+
+        $('.js-show-hide-details--hide').on('click', function () {
+            $(this).closest('.js-show-hide-details__parent').removeClass('deal-option-card__details--show-interactive-block');
+        });
+
+    }
+
+    $(window).on('load', init);
+
+}());

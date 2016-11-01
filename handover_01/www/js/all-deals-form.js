2bbd8677da735f42293e40b78dec8d230abd33ee
dim <web.best.master@gmail.com>
2016-11-01 21:34:41 +0300

    all-deals-form - in progress ...

--- handover_01/www/js/all-deals-form.js
+++ handover_01/www/js/all-deals-form.js
@@ -11,10 +11,33 @@
             return;
         }
 
+        // full form reset
         $('.js-deals-form-section__reset-all').on('click', function () {
             nodeForm.reset();
         });
 
+        // fieldset (section of form) reset only
+        $('.js-deals-form-section__reset-section').on('click', function () {
+
+            $(this).closest('fieldset').find('input, select').each(function () {
+
+                var $node = $(this);
+
+                if ($node.hasClass('js-deals-form-section__select')) {
+                    this.selectedIndex = 0;
+                    $node.selectmenu('refresh');
+                    return;
+                }
+
+                if ($node.hasClass('js-deals-form-section__checkboxradio')) {
+                    this.checked = false;
+                    $node.checkboxradio('refresh');
+                    return;
+                }
+
+            });
+
+        });
 
 
 
